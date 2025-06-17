e8e2035e13be35e06e6d82aafb0f51edfc48f619
kiran <kirannarayan078@gmail.com>
2025-06-17 05:38:10 +0000

    files added

---
+++ app.py
@@ -0,0 +1,11 @@
+num1 = 10
+num2 = 5
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)

4dca59b376270e3b4929a4cfe45f19aae685b639
kiran0113 <kirannarayan7349@gmail.com>
2025-06-17 13:05:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'WELLCOME TO THE KUBEACE WORLD,PROVIDES BEST CLOUD SERVICES !'
+    return 'Hello from Flask running inside Docker!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
